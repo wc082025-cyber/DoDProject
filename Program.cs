0dc8a07981cc758f671a8de83e993f90382cc5b5
wc082025-cyber <wc082025@gmail.com>
2025-09-09 13:59:54 +0200

    initiate commit

--- Program.cs
+++ Program.cs
@@ -1,9 +1,74 @@
-﻿namespace DoDProject;
+﻿using System.Linq.Expressions;
+
+namespace DoDProject;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string input = "";
+        bool validChoice = false;
+        while (!validChoice)
+        {
+
+
+
+            Console.WriteLine("You have woken up in a dungeon. You can see two doors, left and right, and something on the floor");
+            Console.WriteLine("What will you do? go left, go right or pick up?");
+
+            input = Console.ReadLine().Trim().ToLower();
+
+            if (input == "go left")
+            {
+                EnterLeftDoor();
+            }
+
+            {
+                Console.WriteLine("You have chosen the Left door");
+            }
+            else if (input == "go right")
+            {
+                EnterRightDoor();
+            }
+            {
+                Console.WriteLine("you have chosen the Right door");
+            }
+            else if (input == "pick up")
+            {
+                HandlePickup();
+            }
+            {
+                Console.WriteLine("picked up two items, you can only bring one, shield or sword");
+                string itemChoice = Console.ReadLine().Trim().ToLower();
+
+                if (itemChoice == "shield")
+                {
+                    Console.WriteLine("Youo picked up the shield, It is an old wooden shield, not much, but better than nothing");
+
+                }
+                else if (itemChoice == "sword")
+                {
+                    Console.WriteLine("you picked up a sword, it is rusty and probably been here for ages");
+                }
+                else
+                {
+                    Console.WriteLine("Try something else");
+                }
+            }
+
+
+            else
+            {
+                Console.WriteLine("Try again");
+            }
+
+        }
+
+
     }
+    static void EnterLeftDoor()
+        {
+            Console.WriteLine("You Enter the left door, there is a guard. You have a weapon, and kill the guard. Go through the left or right door?");
+            string action = Console.ReadLine().Trim().ToLower();
+        }
 }
--- plan.md
+++ plan.md
@@ -12,6 +12,15 @@
      If time allows and I am able to code the program, I will try to first find a way to randomize order of the rooms. As for my current plan, the rooms have been drawn up as a map with all the answers. EXAMPLE, all rooms are in order, 1R, 1L, 2RR, 2LL as in 2RightRight and 2LeftLeft, are the number of the rooms. I want it to be as when player goes in room 4RR in "vanilla" it has an equal chance of going to door 4LRL. The Naming and structure of rooms will make sense on the visual map. in the second room, player always will get either SWORD or SHIELD.[i have no plans for now to attempt make an inventory for now] If that is aslo very managable, room 3 should have an enemy, where player depending on what what item picked up. The terminal will just write
      (A GUARD APPEARS) with the text (YOU KILLED A GUARD) or (YOUR SHIELD BLOCKED AN ATTACK, YOU COUNTER AND KILLED THE GUARD) depending on what item you got.
 
+     With some additional input from Jhonatan, I will make som QOL changes to the map.
+     The name structure and a visual overhaul to simplify and make readability easier, will be changed.
+
 # pseudocode
 
-    -
+    ```csharp
+        // file: Program.cs
+        // Variables
+        YES/NO
+        LEFT/RIGHT
+
+    // Classes
